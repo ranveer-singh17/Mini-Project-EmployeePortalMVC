df01029f6f200f3d1c8d76d4c12c512c22167560
ranveer-singh17 <93751566+ranveer-singh17@users.noreply.github.com>
2021-11-29 15:00:58 +0530

    commit

---
+++ Procedures/LoginProcedure.sql
@@ -0,0 +1,14 @@
+CREATE proc [dbo].[Usp_Login]( @Username nvarchar(25) , @Password nvarchar(20))
+
+AS
+
+IF Not Exists (Select 1 From LOGIN Where Username = @Username and Password=@Password)
+BEGIN
+	select * from LOGIN where Username=@Username and Password=@Password
+
+RETURN 0
+END
+ELSE
+BEGIN
+RETURN 1
+END
